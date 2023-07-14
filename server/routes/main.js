09b06ca0f7d0201f0417ac464171e9f07d81a98c
Conor Dunne <conordunne@outlook.ie>
2023-07-15 07:42:58 +0900

    Add pagination

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,39 +1,78 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 
-
-router.get("/", async (req, res) => {
-
-    const locals = {
-        title: "NodeJS Blog",
-        description: "A simple blog made with nodeJS"
-    }
-
+router.get('', async (req, res) => {
     try {
-        const data =  await Post.find();
-        res.render("index", {locals, data})
-        console.log(data);
+      const locals = {
+        title: "NodeJs Blog",
+        description: "Simple Blog created with NodeJs, Express & MongoDb."
+      }
+  
+      let perPage = 4;
+      let page = req.query.page || 1;
+  
+      const data = await Post.aggregate([ { $sort: { createdAt: -1 } } ])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+  
+      const count = await Post.count();
+      const nextPage = parseInt(page) + 1;
+      const hasNextPage = nextPage <= Math.ceil(count / perPage);
+  
+      res.render('index', { 
+        locals,
+        data,
+        current: page,
+        nextPage: hasNextPage ? nextPage : null,
+        currentRoute: '/'
+      });
+  
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
+  
+  });
+  
 
-})
-
-// function insertPostData () {
-//       Post.insertMany([
-//         {
-//           title: "blog",
-//           body: "Just testing things out"
-//         },
-//         {
-//             title: "test",
-//             body: "Just testing things out again"
-//           },
-//     ])
+// function insertPostData() {
+//   Post.insertMany([
+//     {
+//       title: "blog",
+//       body: "Just testing things out",
+//     },
+//     {
+//       title: "test",
+//       body: "Just testing things out again",
+//     },
+//     {
+//       title: "blog",
+//       body: "Just testing things out",
+//     },
+//     {
+//       title: "test",
+//       body: "Just testing things out again",
+//     },
+//     {
+//       title: "blog",
+//       body: "Just testing things out",
+//     },
+//     {
+//       title: "test",
+//       body: "Just testing things out again",
+//     },
+//     {
+//       title: "blog",
+//       body: "Just testing things out",
+//     },
+//     {
+//       title: "test",
+//       body: "Just testing things out again",
+//     },
+//   ]);
 // }
 
 // insertPostData();
-
 
 module.exports = router;
